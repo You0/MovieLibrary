c27558e12c23c8b83fed5202c092bf82128d2b72
You0 <1109245765@qq.com>
2017-07-24 22:23:06 +0800

    新建项目
    xxxx

---
+++ app/src/main/java/me/com/movielibrary/ui/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package me.com.movielibrary.ui.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import me.com.movielibrary.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
